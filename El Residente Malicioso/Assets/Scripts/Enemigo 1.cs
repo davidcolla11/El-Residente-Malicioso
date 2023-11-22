5d02ca305f769daf55d72995fff08e5e2531e73b
davidcolla11 <davidcollazostafur@gmail.com>
2023-11-22 05:31:23 -0500

    Enemigo sigue al jugador

--- El Residente Malicioso/Assets/Scripts/Enemigo 1.cs
+++ El Residente Malicioso/Assets/Scripts/Enemigo 1.cs
@@ -4,15 +4,73 @@ using UnityEngine;
 
 public class Enemigo1 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int rutina;
+    public float cronometro;
+    public Animator ani;
+    public Quaternion angulo;
+    public float grado;
+
+    public GameObject target;
     void Start()
     {
-        
+        ani = GetComponent<Animator>();
+        FindTarget();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FindTarget()
     {
-        
+        target = GameObject.Find("Survivor"); // Buscar el objeto por su nombre "Survivor"
+        if (target == null)
+        {
+            Debug.LogWarning("No se encontró el objetivo 'Survivor'");
+        }
+    }
+
+         public void Comportamiento_Enemigo()
+         {
+            if(Vector3.Distance(transform.position, target.transform.position) > 4)
+        {
+            cronometro += 1 * Time.deltaTime;
+            if (cronometro >= 4)
+            {
+            rutina = Random.Range(0, 2);
+                   cronometro = 0;
+            }
+            switch (rutina)
+            {
+                case 0:
+                   ani.SetBool("walk", false);
+                break;
+    
+                case 1:
+                grado = Random.Range(0,360);
+                angulo = Quaternion.Euler(0, grado, 0);
+                rutina++;
+                    break;
+    
+                   case 2:
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, angulo, 0.5f);
+                transform.Translate(Vector3.forward * 1 * Time.deltaTime);
+                ani.SetBool("walk", true);
+                    break;
+            }
+                
+        }
+        else
+     {
+        var lookPos = target.transform.position - transform.position;
+        lookPos.y = 0;
+        var rotation = Quaternion.LookRotation(lookPos);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 2);
+        ani.SetBool("walk", true);
+
+        transform.Translate(Vector3.forward * 1 * Time.deltaTime);
+     }
+     }
+     
+    
+    void Update()
+    {    
+        Comportamiento_Enemigo();
     }
 }
